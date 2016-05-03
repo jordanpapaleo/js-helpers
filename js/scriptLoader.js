496e6bb384b08706e554eec3a655fe46c49b5ba1
Jordan Papaleo <papaleowebdev@gmail.com>
2016-05-03 08:00:46 -0700

    Update scriptLoader.js

--- js/scriptLoader.js
+++ js/scriptLoader.js
@@ -23,3 +23,21 @@ function loadScript(url, cb){
   script.src = url
   document.getElementsByTagName('head')[0].appendChild(script)
 }
+
+function loadjscssfile(filename, filetype){
+  if (filetype === 'js') {
+    var fileref=document.createElement('script')
+    fileref.setAttribute('type','text/javascript')
+    fileref.setAttribute('src', filename)
+    fileref.setAttribute('async', 'async')
+    fileref.setAttribute('defer', 'defer')
+  } else if (filetype === 'css'){
+    var fileref=document.createElement('link')
+    fileref.setAttribute('rel', 'stylesheet')
+    fileref.setAttribute('type', 'text/css')
+    fileref.setAttribute('href', filename)
+  }
+  if (typeof fileref !== 'undefined') {
+    document.getElementsByTagName('head')[0].appendChild(fileref)
+  }
+}
