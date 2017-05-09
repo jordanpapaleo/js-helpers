59c14b38046197c7b4c2e9e0c5e7fe1ab20935b1
Jordan Papaleo <papaleowebdev@gmail.com>
2017-05-09 14:02:33 -0700

    Update manageQueue.js

--- js/manageQueue.js
+++ js/manageQueue.js
@@ -6,29 +6,17 @@ export default class Queue {
   constructor (queue, maxSimultaneous = 3) {
     this.queue = queue
     this.maxSimultaneous = maxSimultaneous
-    this.reporters = []
-  }
-
-  // Function used to emit updates from the queue processing
-  subscribe (cb) {
-    this.reporters.push(cb)
   }
 
   addItem (item) {
     this.queue.push(item)
   }
 
-  report (data) {
-    this.reporters.forEach((reporter) => {
-      reporter(data)
-    })
-  }
-
   /*
    * @param processCb: function returning a promise that does the thing
    */
   processQueue (processCb) {
-    const {queue, maxSimultaneous, report} = this
+    const {queue, maxSimultaneous} = this
     return new Promise((resolve, reject) => {
       let returnData = []
       let running = 0
@@ -40,7 +28,6 @@ export default class Queue {
           processCb(queue.shift()).then(
             data => {
               returnData.push(data)
-              report(data)
               checkQueue()
             },
             err => { reject(err) }
